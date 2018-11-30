12c052febe81a4d62bd519dd7174937bd52b3d08
angsherpa232 <angsherpa232@gmail.com>
2018-11-30 16:24:34 +0100

    /build removed from gitignore of client

---
+++ client/build/precache-manifest.0e66da7a462f791aa25dfbef515c82c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d4c8765b4febc7db53d2",
+    "url": "/static/js/main.d4c8765b.chunk.js"
+  },
+  {
+    "revision": "bbd660da22fe82744c9d",
+    "url": "/static/js/1.bbd660da.chunk.js"
+  },
+  {
+    "revision": "252e57a77b882db5e6d55738f76d0958",
+    "url": "/index.html"
+  }
+];
